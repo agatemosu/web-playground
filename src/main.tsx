4722c5300a9b50e97c1bc4be5d4c06303af45eb9
agatemosu <agaatem@outlook.com>
2025-02-22 13:02:12 +0100

    Use static import for pretty-format

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { render } from "preact";
+import * as prettyFormat from "pretty-format";
 import { App } from "./app";
 import "./styles.css";
 
-window.prettyFormat = await import("pretty-format");
+window.prettyFormat = prettyFormat;
 render(<App />, document.querySelector("#app")!);
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	build: { target: "esnext" },
 	plugins: [preact(), tailwindcss()],
 });
