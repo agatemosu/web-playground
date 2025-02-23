ffcf108109c49992a49d6ad936c4421f7cad148b
agatemosu <agaatem@outlook.com>
2025-02-23 11:53:19 +0100

    Move css link to html

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { render } from "preact";
 import * as prettyFormat from "pretty-format";
 import { App } from "./app";
-import "./styles.css";
 
 window.prettyFormat = prettyFormat;
 render(<App />, document.querySelector("#app")!);
