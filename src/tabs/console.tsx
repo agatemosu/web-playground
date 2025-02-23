b924a671633fef03c6a0cb8c9195aa0d89e5655e
agatemosu <agaatem@outlook.com>
2025-02-23 12:06:55 +0100

    Add icons to console

--- src/tabs/console.tsx
+++ src/tabs/console.tsx
@@ -1,4 +1,20 @@
-import type { Log } from "../console/types";
+import { clsx } from "clsx";
+import type { LucideIcon } from "lucide-preact";
+import { AlertTriangle, Bug, Info, Pencil, XCircle } from "lucide-preact";
+import { h } from "preact";
+import type { Log, Method } from "../console/types";
+
+const Icon = ({ name }: { name: Method }) => {
+	const icons: Record<Method, LucideIcon> = {
+		log: Pencil,
+		info: Info,
+		warn: AlertTriangle,
+		error: XCircle,
+		debug: Bug,
+	};
+
+	return h(icons[name], { size: 16, style: { flexShrink: 0 } });
+};
 
 interface Props {
 	logs: Log[];
@@ -7,9 +23,22 @@ interface Props {
 export const ConsoleTab = ({ logs }: Props) => (
 	<div className="w-full overflow-auto bg-black font-mono">
 		{logs.map((log) => (
-			<p key={log.id} className="border-neutral-800 border-b-1 p-1">
-				{log.message}
-			</p>
+			<div
+				key={log.id}
+				className={clsx(
+					"flex items-center gap-2 border-neutral-800 border-b p-1 pl-2",
+					{
+						"text-green-400": log.prop === "log",
+						"text-blue-400": log.prop === "info",
+						"text-yellow-400": log.prop === "warn",
+						"text-red-400": log.prop === "error",
+						"text-gray-400": log.prop === "debug",
+					},
+				)}
+			>
+				<Icon name={log.prop} />
+				<p className="break-all">{log.message}</p>
+			</div>
 		))}
 	</div>
 );
